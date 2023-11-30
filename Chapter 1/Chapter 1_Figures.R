eba9a2f17df498b4d9ef56ef79630451757323f6
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2023-11-30 21:56:45 +0800

    Chapter 3 Updates

---
+++ Chapter 1/Chapter 1_Figures.R
@@ -0,0 +1,29 @@
+# Figure 4
+# Reproducing the tioh count in the TSM corpus
+fakeMT2015 = data.frame(rep=c(1:10), count=c(3.8,2.6,2.9,2.2,1.9,1.3,0.8,0,0.6,0), 
+                        modelFeet=c(3.8,2.6,2.9,2.2,1.95,1.32,1,0.3,0.1,-1), 
+                        modelFeet=c(3.8,3,2.3,2.1,1.8,1.3,1.1,1,1,0.9))
+
+png(filename = "ch1.figure4.png", width = 1200, height = 900, units = "px",
+    bg = "white", res = 200)
+
+plot(fakeMT2015$count~fakeMT2015$rep, 
+     main = "Repetitions of \"tioh\" in Myers and Tsay (2015)",
+     ylab = "Tokens (base 10 log)", 
+     xlab = "Number of Repetitions", 
+     xaxt = "n", 
+     ylim=c(0,4), lwd = 2, pch = 16, type = "o")
+
+axis(1, at=fakeMT2015$rep, labels=fakeMT2015$rep)
+lines(fakeMT2015$modelFeet~fakeMT2015$rep, pch = 0, type = "o")
+lines(predict(loess(fakeMT2015$modelFeet.1~fakeMT2015$rep)), lty = 2)
+
+legend("topright", 
+       c("Observed", "Model with Feet", "Model without Feet"), 
+       lty = c(1,1,2), 
+       lwd = c(2,1,1), 
+       pch = c(16, 0, NA))
+
+dev.off()
+
+rm(fakeMT2015)
---
+++ Chapter 3/Chapter 3_Figures.R
@@ -0,0 +1,136 @@
+#Figure 11
+dataPoints = c(20, 33, 30, 44, 18, 24)
+dataLabel = c(1, 2, 3, 4, 5, 6)
+
+badLineData = data.frame(Score = dataPoints, Labels = dataLabel)
+
+png(filename = "ch3.figure11.png", width = 1200, height = 900, units = "px",
+    bg = "white", res = 200)
+
+plot(badLineData$Score~badLineData$Label, 
+     main = "不適當的折線圖範例",
+     ylab = "分數", 
+     xlab = "受試者編號", 
+     ylim = c(0, 60),
+     lwd = 2, pch = 16, type = "o")
+
+dev.off()
+
+# Figure 12a
+png(filename = "ch3.figure12a.png", width = 1200, height = 900, units = "px",
+    bg = "white", res = 200)
+
+barplot(badLineData$Score ~ badLineData$Label, 
+     main = "適當的長條圖範例(分數與受試者編號順序無相關性)",
+     ylab = "分數", 
+     xlab = "受試者編號", 
+     ylim = c(0, 60))
+
+dev.off()
+
+# Figure 12b
+dataPoints = c(80, 60, 45)
+dataLabel = c("動詞", "名詞", "其他")
+
+goodBarPlotData = data.frame(Score = dataPoints, Labels = dataLabel)
+
+png(filename = "ch3.figure12b.png", width = 1200, height = 900, units = "px",
+    bg = "white", res = 200)
+
+barplot(goodBarPlotData$Score ~ goodBarPlotData$Label, 
+        main = "適當的長條圖範例(詞頻變化與詞類之間順序無相關性)",
+        ylab = "數量", 
+        xlab = "詞類", 
+        ylim = c(0, 100))
+
+dev.off()
+
+# Figure 14
+set.seed(100)
+fakeAge = round(runif(100) * 100)
+set.seed(200)
+fakeVocSize = round(runif(100) * 30000)
+
+fakeAgeVocCor = data.frame(Age = fakeAge, VocSize = fakeVocSize)
+
+png(filename = "ch3.figure14.png", width = 1200, height = 900, units = "px",
+    bg = "white", res = 200)
+
+plot(fakeAgeVocCor$VocSize ~ fakeAgeVocCor$Age,
+     main = "年齡與字彙量散佈圖(根據隨機資料)",
+     ylab = "字彙量", 
+     xlab = "年齡", 
+     lwd = 2, pch = 1, cex = 1.5, type = "p")
+
+dev.off()
+
+# Figure 16
+RTs = c(670, 739, 780, 653)	
+results.mat = matrix(RTs, nrow = 2)
+rownames(results.mat) = c("nouns","verbs") 		
+colnames(results.mat) = c("Exp. 1","Exp. 2")	
+
+png(filename = "ch3.figure16.png", width = 1200, height = 900, units = "px",
+    bg = "white", res = 200)
+
+barplot(results.mat,			
+	beside=TRUE,				
+	names.arg=c("Exp. 1","Exp. 2"),	
+	legend.text=c("noun","verb"),	
+	ylim = c(0,1100),			
+	ylab = "RT (ms)"			
+)
+
+dev.off()
+
+# Figure 17
+jabberwocky = readLines("Jabberwocky_OnlyWords.txt")
+jw.table = sort(table(jabberwocky), decreasing=TRUE)
+
+png(filename = "ch3.figure17.png", width = 2000, height = 900, units = "px",
+    bg = "white", res = 200)
+
+barplot(jw.table, beside=T, names.arg=names(jw.table), 
+        ylab = "Token counts", main="Jabberwocky word frequencies", 
+        xlab = "Sample of words")
+
+dev.off()
+
+# Figure 18
+png(filename = "ch3.figure18.png", width = 1200, height = 900, units = "px",
+    bg = "white", res = 200)
+
+plot(x = 1:20, y = (1:20)^2,
+     pch = 1, cex = 1.5, type = "p")
+
+dev.off()
+
+# Figure 19
+jw.words = names(jw.table)	
+jw.freq = as.vector(jw.table)	
+word.lens = nchar(jw.words)	
+
+png(filename = "ch3.figure19.png", width = 1200, height = 900, units = "px",
+    bg = "white", res = 200)
+
+plot(x = as.vector(jw.table), y = nchar(jw.words),
+     pch = 1, cex = 1.5, type = "p")
+
+dev.off()
+
+# Figure 20
+png(filename = "ch3.figure20.png", width = 1200, height = 900, units = "px",
+    bg = "white", res = 200)
+
+plot(x = 1:20, y = (1:20)^2, pch = 1:20,
+     cex = 1.5, type = "p") 
+
+dev.off()
+
+# Figure 21
+png(filename = "ch3.figure21.png", width = 1200, height = 900, units = "px",
+    bg = "white", res = 200)
+
+plot(x = 1:20, y = (1:20)^2, type = "l", lwd = 2)
+
+dev.off()
