d220a26adc3224a8ee9375768143748652f0c08f
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2023-12-04 17:56:05 +0800

    Chapter 6 Update

--- Chapter 6/Chapter 6.R
+++ Chapter 6/Chapter 6.R
@@ -83,3 +83,144 @@ cor(fd$LogFreq, fd$Dur)
 
 # 使用cor.test()檢測對數詞頻與字詞時長的微弱負相關性是否有統計上的顯著性
 cor.test(fd$LogFreq, fd$Dur)
+
+# 二之三節
+# 練習三
+IX.z = (fakecor$IX - mean(fakecor$IX)) / sd(fakecor$IX) # 轉換為z分數
+IY.z = (fakecor$IY - mean(fakecor$IY)) / sd(fakecor$IY) # 轉換為z分數
+# 套用皮爾遜相關係數公式得到r
+I.r = sum(IX.z * IY.z) / (length(IX.z) - 1)
+I.r
+# [1] 0.896903362335511
+
+my.test = cor.test(BX, BY)	# 將相關性測試結果儲存至my.test物件
+my.test$estimate			# 取得相關係數
+#cor 
+#-0.9716553
+my.test$statistic			# 取得t值
+#t 
+#-17.43801
+my.test$parameter			# 取得自由度
+#df
+#18
+my.test$p.value			# 取得p值
+#[1] 1.012081e-12
+
+# 練習四
+# 樣本數
+n = length(fakecor$BX)
+# 將原始資料轉換為z分數
+BX.z = scale(fakecor$BX)
+BY.z = scale(fakecor$BY)
+# 套用相關係數公式
+B.r = sum(BX.z * BY.z) / (n - 1)
+# 套用相關係數的t值公式
+B.t = B.r / (sqrt(1 - B.r ^ 2) / sqrt(n - 2))
+# t值為負數，所以雙尾p值就是pt()給出的左尾p值乘2即可
+pt(B.t, df = n - 2) * 2
+# [1] 1.012081e-12
+
+# 縮小fakecor的BX與BY的樣本數，再次計算相關係數的統計顯著性
+# 設定亂數種子，以便我們進行相同的隨機抽樣
+set.seed(2)				
+# 隨機從fd物件中的Word欄位取出100筆資料
+fd100.items = sample(fd$Word, 100)	
+# 從fd物件中取出fd的Word欄位有出現在fd100.items裡的子集合
+fd100 = subset(fd, is.element(fd$Word, fd100.items))
+# p值不顯著
+cor.test(fd100$LogFreq, fd100$Dur)
+
+# 二之四節
+# 建立原始時間序列(按鍵順序)
+x = 
+   c(1,1,0,1,0,1,0,1,1,0,0,1,0,1,0,1,1,0,0,0,1,1,0,1,0,1,0,1,0,0,1,0,1,0,
+     1,0,1,0,1,0,0,0,1,1,0,1,0,0,0,1,1,0,1,0,1,1,0,1,0,1,0,0,1,1,0,1,1,0,
+     1,0,0,1,1,0,0,0,1,1,0,1,1,1,0,0,1,1,0,1,0,0,0,0,1,1,1,0,1,0,1,0,0,1,
+     1,0,0,1,0,0,1,1,0,1,0,1,1,0,1,0,0,0)	
+# 將x的最後一筆資料做為第一筆資料，再與剩下的x所有資料結合(延遲序列)
+# x[length(x)] = 因為length(x)得到的是x的長度數值，所以將這個數值放入x[]
+# 就能夠取得x的最後一筆資料
+# x[1:length(x) - 1] = 按照上面的邏輯"1:length(x) - 1"代表的從"1"到"x長度-1"
+# 的數值向量，所以把這個向量放入x[]，就能得到x的第一筆到倒數第二筆的資料
+# 將最後一筆資料放在其他資料之前用c()結合，就得到了延遲序列
+y = c(x[length(x)], x[1:length(x) - 1])
+cor.test(x,y)
+
+# 三之一節
+plot(AX, AY)			# 使用scatterplots.txt裡的A組資料製作分佈圖
+regress.line = lm(AY ~  AX)	# 產生y = a + bx的線性模型
+abline(regress.line)		# 依線性模型方程式產生理想的迴歸線加到分佈圖上
+
+library(ggplot2)
+# 利用ggplot2產生相同的散佈圖
+# 資料一樣是fakecor，而mapping的部份則分別將AX與AY對應至X軸與Y軸
+ggplot(fakecor, aes(x = AX, y = AY)) + 
+  # 加上每個資料點
+  geom_point() + 
+  # 以線性代數計算並加上迴歸線
+  # se = 0的參數設定是省略繪製迴歸線的「標準誤」，稍後說明
+  geom_smooth(method = "lm", se = 0) +
+  theme_bw()	
+
+# 利用類似的方式產生詞頻與時長的散佈圖與線性迴歸線
+plot(fd$LogFreq, fd$Dur, xlab = "Log Frequency", ylab = "Duration(ms)")
+# 利用lm()的參數data指定使用的資料物件，就可以在Y ~ X的語法中使用物件欄位名稱了
+abline(lm(Dur ~ LogFreq, data = fd))
+
+# 練習五
+# 以ggplot2重製圖十一
+library(ggplot2)
+# 資料指定為fd，而mapping的部份則分別將LogFreq與Duration對應至X軸與Y軸
+ggplot(fd, aes(x = LogFreq, y = Dur)) + 
+  # 加上每個資料點
+  geom_point() + 
+  # 以線性代數計算並加上迴歸線
+  # se = 0的參數設定是省略繪製迴歸線的「標準誤」，稍後說明
+  geom_smooth(method = "lm", se = 0) +
+  # 以labs()函數加上X軸與Y軸的標題
+  labs(x = "Log Frequency", y = "Duration (ms)") +
+  theme_bw()	
+
+# 以predict()函數從線性迴歸模型中產生預測值
+# 建立一個新的資料框，唯一一個欄位與線性迴歸模型中的X變量名稱相同(注意大小寫！)
+# 這個欄位中只包含一個值：8
+newfd = data.frame(LogFreq = 8)
+# 以新的資料框中的X變量皆由線性迴歸模型預測Y變量(時長)的值
+predict(lm(Dur ~ LogFreq, data = fd), newfd)
+#1 
+#243.0067
+# 建立另一個新的資料框，這次代表X變量欄位中包含8至10三個值
+newfd.2 = data.frame(LogFreq = 8:10) 
+
+# 比較「完美」的模型與單純的線性模型
+plot(AX, AY)		# 產生「scatterplots.txt」中A組資料的相關分佈圖
+# 加上線型模型的迴歸線，調整寬度與樣式
+abline(lm(AY ~ AX), lwd = 2, lty = 1)	
+lines(AX, AY, lty = 2)	# 加上完美符合每個資料點的曲折線
+
+# 再次以上面的資料框中的X變量皆由線性迴歸模型預測Y變量(時長)的值
+predict(lm(Dur ~ LogFreq, data = fd), newfd.2)
+
+# 三之二節
+# 讀取regex檔案成為regdat資料框
+regdat = read.delim("regex.txt")
+# 檢視regdat物件裡的欄位名稱
+colnames(regdat)				
+#[1] "x" "y"
+# 使用regdat建立y ~ x的線性迴歸模型
+regdat.lm = lm(y ~ x, data = regdat)	
+regdat.lm
+
+plot(regdat$x, regdat$y, xlab = "x", ylab = "y")	# 產生xy散佈圖
+abline(regdat.lm) # 加上迴歸線
+
+# 呈現迴歸模型的摘要
+summary(regdat.lm)
+
+# 在相關性測試中發現與迴歸模型中斜率相同的相關係數p值
+cor.test(regdat$x, regdat$y)
+
+# 以anova()針對迴歸模型採用變異數分析，發現x的p值也和在迴歸模型中相同
+anova(y ~ x, data = regdat)
+
+# 三之三節
--- Chapter 6/Chapter 6_Figures.R
+++ Chapter 6/Chapter 6_Figures.R
@@ -50,3 +50,94 @@ png(file = "ch6.figure5.png", height = 900, width = 1200, units = "px",
 plot(fd$LogFreq, fd$Dur, xlab = "Log Frequency", ylab = "Duration (ms)")
 
 dev.off()
+
+# Figure 6
+fakecor = read.delim("scatterplots.txt")	# 將資料讀取為fakecor變數
+
+png(file = "ch6.figure6.png", height = 900, width = 1800, units = "px",
+    res = 200)
+
+par(mfrow = c(1, 3))
+
+plotCor = function(x, y, title) {
+  plot(x, y, main = paste(title, "r =", round(cor(x, y), 3)))
+}
+
+plotCor(fakecor$FX, fakecor$FY, "F;")
+plotCor(fakecor$GX, fakecor$GY, "G;")
+plotCor(fakecor$HX, fakecor$HY, "H;")
+
+par(mfrow = c(1, 3))
+
+dev.off()
+
+# Figure 7
+
+png(file = "ch6.figure7.png", height = 900, width = 1200, units = "px",
+    res = 200)
+
+plotCor(fakecor$IX, fakecor$IY, "I;")
+
+dev.off()
+
+# Figure 8
+
+png(file = "ch6.figure8.png", height = 900, width = 1200, units = "px",
+    res = 200)
+
+plotCor(fakecor$EX, fakecor$EY, "E;")
+
+dev.off()
+
+# Figure 9
+png(file = "ch6.figure9.png", height = 900, width = 1200, units = "px",
+    res = 200)
+
+plot(AX, AY)
+regress.line = lm(AY ~ AX)
+abline(regress.line)		
+
+dev.off()
+
+# Figure 10
+library(ggplot2)
+
+ggplot(fakecor, aes(x = AX, y = AY)) + 
+  geom_point() + 
+  geom_smooth(method = "lm", se = 0) +
+  theme_bw()	
+
+ggsave(filename = "ch6.figure10.png", height = 900, width = 1200, units = "px",
+       dpi = 200)
+
+# Figure 11
+png(file = "ch6.figure11.png", height = 900, width = 1200, units = "px",
+    res = 200)
+
+plot(fd$LogFreq, fd$Dur, xlab = "Log Frequency", ylab = "Duration(ms)")
+abline(lm(Dur ~ LogFreq, data = fd))
+
+dev.off()
+
+# Figure 12
+png(file = "ch6.figure12.png", height = 900, width = 1200, units = "px",
+    res = 200)
+
+plot(AX, AY)	
+abline(lm(AY ~ AX), lwd = 2, lty = 1)	
+lines(AX, AY, lty = 2)
+
+dev.off()
+
+# Figure 13
+
+regdat = read.delim("regex.txt")
+regdat.lm = lm(y ~ x, data = regdat)
+
+png(file = "ch6.figure13.png", height = 900, width = 1200, units = "px",
+    res = 200)
+
+plot(regdat$x, regdat$y, xlab = "x", ylab = "y")
+abline(regdat.lm)
+
+dev.off()
