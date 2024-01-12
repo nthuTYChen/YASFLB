7f4a602708d9d0cc3f7a5c853417d962e07a9951
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2024-01-12 16:27:49 +0800

    Chapter 12 Updates

--- Chapter 12/Chapter 12.R
+++ Chapter 12/Chapter 12.R
@@ -65,6 +65,35 @@ chisq.test(cheeseNouns.tab)
 cheese.glm = glm(cheeseNouns ~ 1, family = "binomial")
 summary(cheese.glm) 
 
+# 練習二
+# 先重新建立富豪型刪除的邏輯迴歸模型
+rd = read.delim("richdeletion.txt")
+rd.glm = glm(Deletion ~ Income, family = "binomial", data = rd)
+# 取出收入斜率
+coefs = coef(rd.glm)
+coef.inc = coefs["Income"]
+# coef.inc為0.000554，也就是每當收入增加1，火星人呈現富豪型刪除(P1)的對數勝率
+# 就增加0.000554
+# 手動計算收入為35790時火星人呈現富豪型刪除的預測機率
+# 先以迴歸方程式得出預測的P1對數勝率
+del.logit = coefs["(Intercept)"] + 35790 * coef.inc  # -5.85
+# 再以gtools套件的inv.logit()將預測的對數勝率轉換回機率
+library(gtools)
+inv.logit(del.logit)                                 # 0.00286033，大約0.2%的機率
+
+# 練習三
+# 要使用predict()根據某個自變量的值取得因變量的值，第一步是要建立一個資料框，而欄位
+# 名稱必須與迴歸模型中的該自變量名稱相同，而在這個練習裡就是Income。另外也還記得
+# 怎麼利用seq()函數產生一個每兩個數值之間有著相同間隔的連續數字嗎？
+pred.df = data.frame(Income = seq(from = 20000, to = 60000, by = 1000))
+# 使用建立的資料框和rd.glm從predict()中取得富豪型刪除的預測「對數勝率」
+pred.logits = predict(rd.glm, pred.df)
+# 再次利用gtools套件的inv.logit將對數勝率轉換為機率
+library(gtools)
+# 也可以進位至小數點第三位，比較好解讀數據
+round(inv.logit(pred.logits), 3)
+
+# 二之三之二
 # 從邏輯迴歸模型的檢定報告中取出對數似然值、以內建的「logLik()」函數取出殘餘偏差值
 # 最後再進行<公式五>的驗算
 logLik(cheese.glm)               			# 直接從模型中取出對數似然值
@@ -107,3 +136,113 @@ cheese.chisq$statistics 				# 只取出報告中的卡方值
 #3.333333
 logit.z ^ 2						# 計算邏輯迴歸截距z值平方
 #[1] 3.203021 
+
+# 二之三之三節
+# 以火星人起司名詞為例進行以卡方檢定偏差分析為基礎的似然比檢定
+cheese.glm0 = glm(cheeseNouns ~ 0, family = "binomial")	# 沒有截距的模型
+anova(cheese.glm0, cheese.glm, test = "Chisq")	        # 與有截距模型比較
+
+# 二之四節
+# 假設火星人起司名詞後綴出現與詞頻負相關
+Suffixed = c(rep(1, 20), rep(0, 10))	# 我們的二元因變量
+Frequency = 1:30			   	            # 詞頻數據
+cor.test(Suffixed, Frequency)	        # r = -.82
+
+set.seed(1)				                    # 指定亂數種子
+WordLength = sample(1:30)	            # 隨機從1-30再抽樣隨機排序數字
+
+# 直接指定每個數據向量(而不是資料框中的欄位)做為因變量和自變量
+cheese2.glm = glm(Suffixed ~ Frequency + WordLength, family = "binomial")
+#Warning messages:
+#  1: glm.fit: algorithm did not converge 
+#  2: glm.fit: fitted probabilities numerically 0 or 1 occurred
+summary(cheese2.glm)
+
+# 在因變量與自變量的相關性加入一點雜訊，使得迴歸模型不再百分百完美
+# 取出原始Suffixed變量的第2個到第30個值，再取出原始Suffixed變量的第1個值
+# 然後用c()重新結合，將原始的第1個值變成新的變量中的最後一個值
+Suffixed.f = c(Suffixed[2:30], Suffixed[1])
+cheese3.glm = glm(Suffixed.f ~ Frequency + WordLength, family = "binomial")
+summary(cheese3.glm)
+
+# 練習四
+# 先建立只包含詞頻的起司名詞後綴模型
+cheese.glm.freq = glm(Suffixed.f ~ Frequency, family = "binomial")
+# 也建立使用Suffixed.f做為因變量的只含截距模型
+cheese.glm0.f = glm(Suffixed.f ~ 1, family = "binomial")
+# 與完整模型(使用Suffixed.f做為因變量)的似然比檢定
+# 缺少/增加單詞時長自變量並無顯著改變模型配適度
+anova(cheese3.glm, cheese.glm.freq, test = "Chisq")
+# 與只含有截距的模型進行似然比檢定，加入詞頻顯著減少殘餘值/增加配適度
+anova(cheese.glm0.f, cheese.glm.freq, test = "Chisq")
+
+# 二之五節
+# 以火星人富豪型刪除資料示範正確的二元資料模型圖表繪製
+rd = read.delim("richdeletion.txt")
+rd.glm = glm(Deletion ~ Income, family = "binomial", data = rd)
+
+# 繪製符合因變量二元原始資料的最佳配適曲線
+options(scipen = 5)		        # 強迫R以5位數整數而非e次方格式呈現數值
+plot(rd$Deletion ~ rd$Income)	# 繪製散佈圖
+rd.pred = predict(rd.glm, type = "response")	# 取得原始因變量的估計值
+# 將收入數值與富豪型刪除估計值合併為一個資料框
+curve.df = data.frame(Income = rd$Income, fitted = rd.pred)
+# 再將整個資料框根據收入從小到大排序列，以便可以使用lines()函數依序將估計值按照
+# 收入從小到大排序的收入數值連成一條曲線
+curve.df = curve.df[order(curve.df$Income), ]
+lines(x = curve.df$Income, y = curve.df$fitted)		
+
+# 練習五
+library(ggplot2)
+ggplot(data = rd, mapping = aes(x = Income, y = Deletion)) + 
+  # 加上實際數據點時設定為半透明，可以突顯最佳配適線
+  geom_point(alpha = .5) +
+  stat_smooth(method = "glm", se = FALSE, method.args = list(family = "binomial")) +
+    theme_bw()
+
+# 以X軸與Y軸分組計算平均數，解決呈現個別數據點與機率變化趨勢的問題
+# 將ｘ軸自變量放入十個範圍相同的分組
+bins = cut(rd$Income, 10) 			
+# 每個範圍分組裡算出一個收入平均數
+mean.x = tapply(rd$Income, bins, mean) 	
+# 計算每個範圍分組裡的呈現富豪型刪除與否的平均數，也就是呈現刪除的機率
+prob.y = tapply(rd$Deletion, bins, mean)	
+# 重覆繪圖的過程
+options(scipen = 5)
+# 使用經過分組計算的數值繪製散佈圖
+plot(mean.x, prob.y, ylab = "Deletion rate", xlab = "Income")
+# 以稍早產生的收入數值對應的估計機率的資料加上最佳配適曲線
+lines(x = curve.df$Income, y = curve.df$fitted)
+
+# 練習六
+bins.df = data.frame(Income = mean.x, Prob = prob.y)
+
+library(ggplot2)
+# ggplot()函數不做任何對應，只建立空白底圖
+ggplot() + 
+  # 在geom_point()函數中設定分組計算資料框為資料來源
+  geom_point(data = bins.df, mapping = aes(x = Income, y = prob.y), alpha = .5) +
+  # 在stat_smooth()函數中設定模型估計值資料來源
+  stat_smooth(data = rd, mapping = aes(x = Income, y = Deletion),
+              method = "glm", se = FALSE, method.args = list(family = "binomial")) +
+  theme_bw()
+
+# 直接以對數勝率呈現線性的最佳配適線
+bins = cut(rd$Income, 10)		# 同樣將X軸進行範圍分組
+# 建立Y軸進行範圍分組且加入雜訊的函數
+logit.bin = function(vector) {
+ 	prob1 = mean(c(vector, 0, 1)) 	# 為每個分組的P1計算多增加兩個雜訊數值
+	prob0 = 1 - prob1 		         	# 計算範圍分組的P0
+	log.odds = log(prob1 / prob0) 	# 加了雜訊後P1和P0都不可能是0
+	return(log.odds)			          # 回傳分組對數勝率
+}
+mean.x = tapply(rd$Income, bins, mean) 	# 為每個X軸分組計算平均數
+# 為每個Y軸分組以logit.bin計算平均數
+logit.y = tapply(rd$Deletion, bins, logit.bin)	
+# 產生散佈圖
+plot(mean.x, logit.y, ylab = "Deletion (log odds)", xlab = "Income")
+abline(lm(logit.y ~ mean.x)) 			      # 加上線性迴歸最佳配適線
+
+# 以effects套件繪製圖表，結合圖五及圖六的優點
+library(effects)			      # 記得先安裝載入套件
+plot(allEffects(rd.glm))		# 取得rd.glm模型中的所有效應繪製圖表
--- Chapter 12/Chapter 12_Figures.R
+++ Chapter 12/Chapter 12_Figures.R
@@ -32,3 +32,64 @@ png(filename = "ch12.figure3.png", width = 1200, height = 900, unit = "px",
 curve(logit(x), 0, 1)
 
 dev.off()
+
+# Figure 4
+rd = read.delim("richdeletion.txt")
+rd.glm = glm(Deletion ~ Income, family = "binomial", data = rd)
+
+png(filename = "ch12.figure4.png", width = 1200, height = 900, unit = "px",
+    res = 200)
+
+options(scipen = 5)		        
+plot(rd$Deletion ~ rd$Income)	
+rd.pred = predict(rd.glm, type = "response")
+curve.df = data.frame(Income = rd$Income, fitted = rd.pred)
+curve.df = curve.df[order(curve.df$Income), ]
+lines(x = curve.df$Income, y = curve.df$fitted)	
+
+dev.off()
+
+# Figure 5
+png(filename = "ch12.figure5.png", width = 1200, height = 900, unit = "px",
+    res = 200)
+
+bins = cut(rd$Income, 10) 			
+mean.x = tapply(rd$Income, bins, mean) 	
+prob.y = tapply(rd$Deletion, bins, mean)	
+options(scipen = 5)
+plot(mean.x, prob.y, ylab = "Deletion rate", xlab = "Income")
+lines(x = curve.df$Income, y = curve.df$fitted)
+
+dev.off()
+
+# Figure 6
+bins = cut(rd$Income, 10)	
+logit.bin = function(vector) {
+  prob1 = mean(c(vector, 0, 1)) 
+  prob0 = 1 - prob1 		         
+  log.odds = log(prob1 / prob0) 
+  return(log.odds)			          
+}
+mean.x = tapply(rd$Income, bins, mean) 
+logit.y = tapply(rd$Deletion, bins, logit.bin)	
+
+png(filename = "ch12.figure6.png", width = 1200, height = 900, unit = "px",
+    res = 200)
+
+plot(mean.x, logit.y, ylab = "Deletion (log odds)", xlab = "Income")
+abline(lm(logit.y ~ mean.x)) 			     
+
+dev.off()
+
+# Figure 7
+library(effects)
+
+rd = read.delim("richdeletion.txt")
+rd.glm = glm(Deletion ~ Income, family = "binomial", data = rd)
+
+png(filename = "ch12.figure6.png", width = 1200, height = 900, unit = "px",
+    res = 200)
+
+plot(allEffects(rd.glm))		
+
+dev.off()
