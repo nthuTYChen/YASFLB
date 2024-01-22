dd5ab843a9c46a96e7a4dc942e2183fb70f36118
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2024-01-22 21:51:29 +0800

    Chapter 13

--- Chapter 13/Chapter 13.R
+++ Chapter 13/Chapter 13.R
@@ -122,3 +122,181 @@ for (i in 1:20) {       # 進行20次迴圈
 # 產生線性模型與混合效應模型x係數的相關性散佈圖
 plot(lm.coefs, lme.coefs, main = "Linear Regression vs. LME Coefficients",
      xlab = "Linear Regression X", ylab = "LME X") 
+
+# 計算從模型中得到的跨受測單位的截距與斜率的平均數，得到混合效應模型的最佳配適線
+coefs = coef(sdat.lmer) # 從混合效應模型中取得跨測量單位的截距與斜率
+head(coefs$Child)       # 數據以Child分組
+# 從截距列表取得截距平均數。「`」符號的功用是將括號做為欄位名稱而非程式碼一部份
+sdat.int.mean = mean(coefs$Child$`(Intercept)`)
+# 從斜率列表取得截距平均數
+sdat.age.mean = mean(coefs$Child$Age)
+# 產生實際數據的散佈圖
+plot(Sploink ~ Age, data = sdat, ylim = c(0, 20))
+# 以線性模型加上最佳配適線，並設定以粗的虛線呈現
+abline(sdat.lm, lty = 2, lwd = 3)
+# 以混合效應模型的測量單位截距平均數(3.75)與斜率平均數(1.45)加上最佳配適線
+abline(a = sdat.int.mean, b = sdat.age.mean)
+# 加上圖例
+legend("topright", lty = c(1, 2), lwd = c(1, 3), legend=c("LME", "ordinary lm"))
+
+# 以effects套件產生的資訊將混合效應模型視覺化
+library(effects)
+sdat.eff = allEffects(sdat.lmer) # 從混合效應模型中取得最佳配適線資訊
+plot(sdat.eff)				# 以plot()繪製模型最佳配適線折線圖
+
+# 練習四
+library(effects)
+# 利用effects套件的effect()函數從混合效應模型中取得Age的效應
+# 再利用as.data.frame()函數將模型資訊轉換為資料框物件
+sdat.eff.df = as.data.frame(effect("Age", sdat.lmer))
+
+library(ggplot2)
+
+# 因為不同的geom_xxxx()函數會使用不同的資料來源，所以ggplot()函數不進行任何
+# 底圖設定
+ggplot() +
+  # 以實際數據sdat加上數據點產生散佈圖
+  geom_point(data = sdat, mapping = aes(x = Age, y = Sploink), 
+             size = 2.5, alpha = .5) +
+  # 以模型Age效應估計值(fit)加上最佳配適線
+  geom_line(data = sdat.eff.df, mapping = aes(x = Age, y = fit),
+            linewidth = 2) +
+  # 以模型Age效應的標準誤計算95%信賴區間，做為繪製誤差範圍的最大與最小值
+  # 並將陰影透明度設定為0.2避免覆蓋了實際數據點
+  geom_ribbon(data = sdat.eff.df, 
+              mapping = aes(x = Age, y = fit, ymin = fit - se * 1.96,
+                            ymax = fit + se * 1.96),
+              alpha = .2) +
+  # 在圖表標題title呈現模型結構，並在註解caption說明陰影範圍代表的意義
+  labs(title = "Sploink ~ Age + (Age | Child)", x = "Age", y = "Sploink", 
+       caption = "Ribbons = 95% CI") +
+  theme_bw()
+
+# 三之一節
+# 以第十章哆拉妹的研究示範同時將兩個隨機變量納入分析
+ddat = read.delim("doramiR.txt", stringsAsFactor = T)	# 檔案也可在第十三章資源中找到
+# 缺少的反應時間資料會在RT欄位中以「NA」呈現，所以使用na.omit()函數排除任何欄位
+# 有著「NA」的資料列
+ddat.clean = na.omit(ddat)
+# 以aov()進行含有兩個隨機變量的變異數分析，產生錯誤訊息		
+ddat.aov = aov(RT ~ Education * SynCat * Freq + 
+  # 受試者編號需以as.factor()轉換為因子
+  # 詞類與詞頻高低都是受試者的組內因子
+ 	Error(as.factor(Participant) / (SynCat * Freq)),
+  # 受試者編號需以as.factor()轉換為因子，教育程度是受測項目的組內因子
+ 	Error(as.factor(Item) / Education), 
+ 	data = ddat)
+
+# 以Clark (1973)的方式進行跨受試者與跨受試項目計算詞頻效應的minF與p值
+attach(ddat) # 設定ddat為常駐物件簡化以下的程式碼 
+# 以受試者為測量單位：
+# 根據每個受試者/教育程度計算對每個詞類和詞頻高低計算反應時間平均數
+ddat.part = aggregate(RT, list(Participant, Education, SynCat, Freq), mean) 
+# 重新依照每個欄位代表的資料重新命名欄位名稱
+colnames(ddat.part) = c("Participant", "Education", "SynCat", "Freq", "RT") 
+# 以受測項目為測量單位：
+# 根據每個受測項目計算不同教育程度對每個詞類和詞頻高低的反應時間平均數
+ddat.item = aggregate(RT, list(Item, Education, SynCat, Freq), mean) 
+# 同上，重新命名各個欄位
+colnames(ddat.item) = c("Item", "Education", "SynCat", "Freq", "RT") 
+detach(ddat) # 利用完常駐狀態的好處後別忘了解除常駐狀態
+# 將受試者編號轉換為因子
+ddat.part$Participant = as.factor(ddat.part$Participant)
+# 進行跨受試者變異數分析，並將檢定報告存入bypart.aov
+bypart.aov = summary(aov(RT ~ Education * SynCat * Freq +
+                         Error(Participant / (SynCat * Freq)), 
+                         data = ddat.part)) 
+# 將受測項目編號轉換為因子
+ddat.item$Item = as.factor(ddat.item$Item)
+# 進行跨受測項目變異數分析，並將檢定報告存入byitem.aov
+byitem.aov = summary(aov(RT ~ Education * SynCat * Freq +
+                         Error(Item / Education), data = ddat.item)) 
+
+# 參照第十章公式十至十四取得變異數分析報告中各項關鍵數據進行minF'計算
+# 報告中的第三個部份第一個表格的第一列第四欄數值，依此類推
+F1.Freq = bypart.aov[[3]][[1]][1, 4]   # 跨受試者分析的詞頻效應F值
+dfnum1 = bypart.aov[[3]][[1]][1, 1]    # 詞頻效應的自由度
+dfdenom1 = bypart.aov[[3]][[1]][5, 1]  # 跨受試者分析殘餘值自由度
+F2.Freq = byitem.aov[[1]][[1]][3, 4]   # 跨受測項目的詞頻效應F值
+dfdenom2 = byitem.aov[[1]][[1]][7, 1]  # 跨受測項目的殘餘值自由度
+# 第十章公式十至十四
+minF = (F1.Freq * F2.Freq) / (F1.Freq + F2.Freq)
+dfnum.minF = dfnum1 
+dfdenom.minF = (F1.Freq + F2.Freq) ^ 2 / (F1.Freq ^ 2 / dfdenom2 + F2.Freq ^ 2 / dfdenom1) 
+# 以minF'和對應的自由度計算F分佈右尾p值
+pf(minF, dfnum.minF, dfdenom.minF, lower.tail = F)
+
+# 重新以lme4進行納入兩個隨機變量的示範
+detach("package:afex", unload = T)	# 先卸載afex套件(假設你還在用他的話)
+library(lme4)
+
+# 將所有的二元類別因子的層次對比總和編碼(sum coding)取代預設的虛擬編碼(dummy coding)
+contrasts(ddat$Education) = contr.sum(levels(ddat$Education)) 
+contrasts(ddat$SynCat) = contr.sum(levels(ddat$SynCat)) 
+contrasts(ddat$Freq) = contr.sum(levels(ddat$Freq)) 
+# 建立混合效應模型：前半部包含了固定效應和交互作用、後半部包含了隨機效應
+ddat.lmer = lmer(RT ~ Education * SynCat * Freq +			
+                   (SynCat * Freq | Participant) + (Education | Item), 
+                 data = ddat)
+
+summary(ddat.lmer)
+
+# 以t = z的假設根據常態分佈計算詞頻效應雙尾p值
+pnorm(-2.598) * 2
+# 利用afex套件以Satterwaite方法進行計算
+detach("package:lme4", unload=TRUE) 
+library(afex) 
+ddat.lmer.p = lmer(RT ~ Education * SynCat * Freq + 
+  (SynCat * Freq|Participant) + (Education|Item), 
+ 	data = ddat)
+summary(ddat.lmer.p)
+
+# 以實際數據與模型估計值計算皮爾遜相關係數二次冪，以呈現模型配適度/效應值
+# 注意這裡是和已經排除NA資料的ddat.clean進行比較
+cor(ddat.clean$RT, predict(ddat.lmer)) ^ 2
+
+# 以MuMIn套件計算更精確的效應值
+library(MuMIn)			     # 安裝和載入時注意名稱大小寫
+r.squaredGLMM(ddat.lmer) # 把混合效應模型放進去函數
+
+# 照著結果指示查看函數說明文件
+?r.squaredGLMM
+
+# 以一般線性迴歸模型示範r2很接近R2m
+ddat.lm = lm(RT ~ Education * SynCat * Freq, data = ddat)
+summary(ddat.lm)	# adjusted r2 = 0.03422
+
+# 以<公式四>計算混合效應模型中的詞頻效應值
+# 分子
+# 注意，這裡使用的資料來源是清除NA列數的ddat.clean
+ddat.highFreq = subset(ddat.clean, Freq == "High")
+ddat.lowFreq = subset(ddat.clean, Freq == "Low")
+Freq.RT.diff = abs(mean(ddat.highFreq$RT) - mean(ddat.lowFreq$RT)) # 約62
+
+# 可以用「lme4」套件的「VarCorr()」函數取得模型中所有效應的變異數
+ddat.lmer.var = VarCorr(ddat.lmer)
+# 用「as.data.frame()」轉換為資料框物件會更容易閱讀這些數據
+ddat.lmer.var = as.data.frame(ddat.lmer.var)
+
+var.int.part = ddat.lmer.var[1, "vcov"]	  # 跨受試者截距變異數
+var.int.item = ddat.lmer.var[11, "vcov"]	# 跨受測項目截距變異數
+var.slope.part = ddat.lmer.var[3, "vcov"]	# 跨受試者詞頻斜率變異數
+var.slope.item = 0 # 每個受測項目的詞頻是固定的，所以沒有跨受測項目的詞頻斜率
+var.resid = ddat.lmer.var[14, "vcov"]		 # 模型未分組殘餘值
+
+# 計算分母
+sd.total = sqrt(var.int.part + var.slope.part + var.slope.item + var.resid)
+sd.total
+
+# 公式四
+cohens.d.val = Freq.RT.diff / sd.total 
+cohens.d.val
+
+# 三之二
+# 以似然比檢定證明隨機斜率的重要性
+sdat = read.delim("sploink.txt")	# 假設需要重新讀取資料
+# 最大化模型
+sdat.lmer = lmer(Sploink ~ Age + (Age | Child), data = sdat) 
+# 只含隨機截距模型
+sdat.lmer.int = lmer(Sploink ~ Age + (1 | Child), data = sdat)
+anova(sdat.lmer.int, sdat.lmer)	# 似然比檢定
--- Chapter 13/Chapter 13_Figures.R
+++ Chapter 13/Chapter 13_Figures.R
@@ -39,3 +39,44 @@ plot(lm.coefs, lme.coefs, main = "Linear Regression vs. LME Coefficients",
      xlab = "Linear Regression X", ylab = "LME X") 
 
 dev.off()
+
+# Figure 4
+library(lme4)
+sdat.lmer = lmer(Sploink ~ Age + (Age | Child), data = sdat)
+
+tiff(filename = "ch13.figure4.tiff", height = 1350, width = 1800, unit = "px",
+     compression = c("lzw"), res = 300)
+
+plot(Sploink ~ Age, data = sdat, ylim = c(0, 20))
+points(sdat$Age, predict(sdat.lmer), pch = 20)
+legend("topright", pch = c(1, 20), legend = c("Observed", "LME predictions")) 
+
+dev.off()
+
+# Figure 5
+sdat.lm = lm(Sploink ~ Age, data = sdat)
+coefs = coef(sdat.lmer)
+head(coefs$Child)
+sdat.int.mean = mean(coefs$Child$`(Intercept)`)
+sdat.age.mean = mean(coefs$Child$Age)
+
+tiff(filename = "ch13.figure5.tiff", height = 1350, width = 1800, unit = "px",
+     compression = c("lzw"), res = 300)
+
+plot(Sploink ~ Age, data = sdat, ylim = c(0, 20))
+abline(sdat.lm, lty = 2, lwd = 3)
+abline(a = sdat.int.mean, b = sdat.age.mean)
+legend("topright", lty = c(1, 2), lwd = c(1, 3), legend=c("LME", "ordinary lm"))
+
+dev.off()
+
+# Figure 6
+library(effects)
+sdat.eff = allEffects(sdat.lmer)
+
+tiff(filename = "ch13.figure6.tiff", height = 1350, width = 1800, unit = "px",
+     compression = c("lzw"), res = 300)
+
+plot(sdat.eff)
+
+dev.off()
